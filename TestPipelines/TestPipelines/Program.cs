1d58462bbb1c31f8dfc8ea68b12a64977cadcce1
Franklin Reyes <alex_andres2009@hotmail.com>
2019-10-14 11:44:49 -0600

    Prueba de proyecto base
    prueba para configurar Pipelines

---
+++ TestPipelines/TestPipelines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPipelines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba completada!");
+        }
+    }
+}
